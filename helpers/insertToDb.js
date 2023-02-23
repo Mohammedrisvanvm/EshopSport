1f74230e1e4b6291d64710813db7aac38f7400e7
Mohammedrisvan <risvanrishu0000@gmail.com>
2023-02-23 20:13:18 +0530

    password bcrypted

--- helpers/insertToDb.js
+++ helpers/insertToDb.js
@@ -1,5 +1,6 @@
 import getDiscriminatorByValue from "mongoose/lib/helpers/discriminator/getDiscriminatorByValue.js";
 import { users } from "../models/userSchema.js"
+import bcrypt from "bcrypt"
 
 const createDocument=async (data)=>{
     console.log(data);
@@ -7,7 +8,9 @@ const createDocument=async (data)=>{
     const userinfo= await users.findOne({email})
     if (userinfo!=email){
     try{
+        data.password=await bcrypt.hash(data.password,10)
         const userData = new users({
+           
             name:data.name,
             email:data.email,
             password:data.password
