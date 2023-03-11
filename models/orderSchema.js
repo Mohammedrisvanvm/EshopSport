4f5dee14e1ea8a11fb8e582953360aa82d65b147
Mohammedrisvan <risvanrishu0000@gmail.com>
2023-03-11 15:13:36 +0530

    order schema

---
+++ models/orderSchema.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    orderStatus: {
+      type: String,
+      default: "pending",
+    },
+    paid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    address: {
+      type: Object,
+      required: true,
+    },
+    product: {
+      type: Object,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: new Date(),
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    dispatch: {
+      type: Date,
+      default: new Date(new Date().setDate(new Date().getDate() + 7)),
+    },
+    payment: {
+      type: Object,
+      default: {},
+    },
+    paymentType: {
+      type: String,
+      default: "cod",
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    amountPayable: {
+      type: Number,
+      default: 0,
+    },
+    coupon: {
+      type: Object,
+      default: { applied: false, price: 0, coupon: {} },
+    },
+    orderId: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+export const orderModel = mongoose.model("order", orderSchema);
+
