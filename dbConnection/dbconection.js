cd2e34f04fc583b937b3f1b9441d2665b8aca83e
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-11-29 22:04:41 +0530

    updated db

--- dbConnection/dbconection.js
+++ dbConnection/dbconection.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 
-function dbConnect() {
-    mongoose.set('strictQuery', false)
-    mongoose.connect(process.env.MONGOOSE_CONNECTLOCAL).then(console.log("eshop db connected  !!!")).catch((err)=>console.log(err))
+async function dbConnect() {
+  mongoose.set('strictQuery', false);
 
-    
+  try {
+    await mongoose.connect(process.env.MONGOOSE_CONNECTLOCAL);
+    console.log("eshop db connected !!!");
+  } catch (err) {
+    console.error(err);
+  }
 }
-export default dbConnect 
+
+export default dbConnect;
